c44efc7f7629379e1018b77ac33b27bc45c4a17b
NathanC90 <ymnchao@gmail.com>
2024-05-01 14:18:07 +0800

    Bonus: Add/remove class to overlay

--- 00-beginner/modal/script.js
+++ 00-beginner/modal/script.js
@@ -14,12 +14,14 @@ const overlay = document.querySelector("#overlay");
 // TODO: 3. Create a click event listener for the open-modal-btn that adds the class "open" to the modal
 openModalBtn.addEventListener("click", () => {
   modal.classList.add("open");
+  overlay.classList.add("open");
 });
 // BONUS: Also add the class "open" to the overlay
 
 // TODO: 4. Create a click event listener for the close-modal-btn that removes the class "open" from the modal
 closeModalBtn.addEventListener("click", () => {
   modal.classList.remove("open");
+  overlay.classList.remove("open");
 });
 // BONUS: Also remove the class "open" from the overlay
 
