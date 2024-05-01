497d4a1eba43dec326410be9b834f96735ec6bf7
NathanC90 <ymnchao@gmail.com>
2024-05-01 11:50:54 +0800

    declare all the variables we need for now and add a new comment regarding adding a new element to the list

--- 00-beginner/todo/script.js
+++ 00-beginner/todo/script.js
@@ -5,6 +5,12 @@ form: #new-item-form
 input: #item-input
 */
 
+const list = document.querySelector("#list")
+const form = document.querySelector("#new-item-form")
+const input = document.querySelector("#item-input")
+
 // 2. Add the item to the list
+// To add a new item to the list, we need to create a new element in the list div
+
 
 // 3. Remove the item by clicking the item itself
