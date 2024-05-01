4c91a37e92f255f06b96bbfe262f3d8a20066c22
NathanC90 <ymnchao@gmail.com>
2024-05-01 11:48:14 +0800

    Add comments regarding buiding steps

---
+++ 00-beginner/todo/script.js
@@ -0,0 +1,10 @@
+// 1. Retrieve all the elements we need
+/*
+list: #list
+form: #new-item-form
+input: #item-input
+*/
+
+// 2. Add the item to the list
+
+// 3. Remove the item by clicking the item itself
