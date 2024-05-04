5875f21afa534367ac4dd93c90aa394bdaf4c4e8
NathanC90 <ymnchao@gmail.com>
2024-05-04 22:20:23 +0800

    Show alert when all answers are correct and remove the alert after 1 sec

--- 00-beginner/quiz-app/script.js
+++ 00-beginner/quiz-app/script.js
@@ -8,6 +8,8 @@
 
 const form = document.querySelector("#quiz-form")
 const answers = document.querySelectorAll(".answer")
+const questions = document.querySelectorAll(".question-item")
+const alert = document.querySelector("#alert")
 
 // TODO: 3. Create a submit event listener for the form that does the following.
 form.addEventListener("submit", (e) => {
@@ -20,17 +22,25 @@ form.addEventListener("submit", (e) => {
             selectedAnswers.push(answer)
         }
     })
-    
     //    3. Loop through the selected answer to see if they are correct or not (Check the value of the answer to see if it is the string "true")
+    let correctAnswer = 0
     selectedAnswers.forEach(selected => {
         if(selected.value === "true") {
             //    4. For each correct answer add the class `correct` to the parent with the class `question-item` and remove the class `incorrect`.
             selected.closest(".question-item").classList.add("correct")
+            correctAnswer = correctAnswer + 1
         } else {
             //    5. For each incorrect answer add the class `incorrect` to the parent with the class `question-item` and remove the class `correct`.
             selected.closest(".question-item").classList.add("incorrect")
         }
+        //    6. BONUS: Make sure unanswered questions show up as incorrect. The easiest way to do this is to add the incorrect class and removing the correct class from all question items before checking the correct answers
+        //    7. BONUS: If all answers are correct show the element with the id `alert` and hide it after one second (look into setTimeout) (use the class active to show the alert and remove the class to hide it)
+        if(correctAnswer === 3) {
+            alert.classList.add("active")
+            setTimeout(function() {
+                alert.classList.remove("active")
+            }, 1000)
+        }
     })
-    //    6. BONUS: Make sure unanswered questions show up as incorrect. The easiest way to do this is to add the incorrect class and removing the correct class from all question items before checking the correct answers
-    //    7. BONUS: If all answers are correct show the element with the id `alert` and hide it after one second (look into setTimeout) (use the class active to show the alert and remove the class to hide it)
+
 })
