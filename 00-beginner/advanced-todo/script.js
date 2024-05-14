9e4519c34e2f6cb342401731bd4eb3fa2551b374
NathanC90 <ymnchao@gmail.com>
2024-05-14 16:43:10 +0800

    Add id so the correct item can be referenced

--- 00-beginner/advanced-todo/script.js
+++ 00-beginner/advanced-todo/script.js
@@ -7,8 +7,15 @@ const template = document.querySelector("#list-item-template");
 const LOCAL_STORAGE_PREFIX = "ADVANCED_TODO_LIST";
 const TODOS_STORAGE_KEY = `${LOCAL_STORAGE_PREFIX}-todos`;
 const todos = loadTodos();
-console.log(todos)
 todos.forEach(todo => renderTodo(todo))
+
+list.addEventListener("change", (e) => {
+    if(!e.target.matches("[data=list-item-checkbox]")) return
+
+    // Get the todo that is clicked on
+    // Toggle the complete property to be equal to the checkbox value
+    // Save our updated todo
+})
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -17,7 +24,8 @@ form.addEventListener("submit", (e) => {
   if (todoName === "") return;
   const newTodo = {
     name: todoName,
-    complete: false
+    complete: false,
+    id: new Date().valueOf().toString()
   }
   todos.push(newTodo);
   renderTodo(newTodo);
@@ -28,6 +36,8 @@ form.addEventListener("submit", (e) => {
 // This should then add todo to the list above.
 function renderTodo(todo) {
   const templateClone = template.content.cloneNode(true);
+  const listItem = templateClone.querySelector(".list-item")
+  listItem.dataset.todoId = todo.id
   const textElement = templateClone.querySelector("[data-list-item-text]");
   textElement.innerText = todo.name;
   list.appendChild(templateClone);
