7ccb259ebaf37920af64802e1ae56f7ebe076b41
NathanC90 <ymnchao@gmail.com>
2024-05-14 23:01:13 +0800

    Make button text change accordingly

--- 00-beginner/expand-collapse/script.js
+++ 00-beginner/expand-collapse/script.js
@@ -2,6 +2,12 @@ const expandButtons = document.querySelectorAll(".expand-button")
 
 expandButtons.forEach(button => {
     button.addEventListener("click", (e) => {
+        let currentButton = e.target
+        if(currentButton.innerHTML === "Expand") {
+            currentButton.innerHTML = "Collapse"
+        } else {
+            currentButton.innerHTML = "Expand"
+        }
         const currentHeader = e.target.closest(".card-header")
         const currentBody = currentHeader.nextElementSibling
         currentBody.classList.toggle("show")
