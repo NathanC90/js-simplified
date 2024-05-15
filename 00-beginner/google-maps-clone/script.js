950f8936950993a0d39660c6ac2bc08a9237a6b6
NathanC90 <ymnchao@gmail.com>
2024-05-15 16:00:32 +0800

    Center map to our location

--- 00-beginner/google-maps-clone/script.js
+++ 00-beginner/google-maps-clone/script.js
@@ -1,9 +1,25 @@
-const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoibmF0ZWZsaXhhbmRjb2RlIiwiYSI6ImNsdzdobXhyeTFnY3kyanFlNnkwOXU4ZnoifQ.nX_ssZ4QjYUGrdv0-CL0Dg'
+const MAPBOX_ACCESS_TOKEN =
+  "pk.eyJ1IjoibmF0ZWZsaXhhbmRjb2RlIiwiYSI6ImNsdzdobXhyeTFnY3kyanFlNnkwOXU4ZnoifQ.nX_ssZ4QjYUGrdv0-CL0Dg";
 
-    const map = new mapboxgl.Map({
-        accessToken: MAPBOX_ACCESS_TOKEN,
-        container: 'map', // container ID
-        style: 'mapbox://styles/mapbox/streets-v12', // style URL
-        center: [-74.5, 40], // starting position [lng, lat]
-        zoom: 9, // starting zoom
-    });
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+  enableHighAccuracy: true,
+});
+
+function setupMap(centerPosition) {
+  const map = new mapboxgl.Map({
+    accessToken: MAPBOX_ACCESS_TOKEN,
+    container: "map", // container ID
+    style: "mapbox://styles/mapbox/streets-v12", // style URL
+    center: centerPosition, // starting position [lng, lat]
+    zoom: 9, // starting zoom
+  });
+}
+
+function successLocation(position) {
+  setupMap([position.coords.longitude, position.coords.latitude]);
+  console.log(position);
+}
+
+function errorLocation() {
+  console.log("Error");
+}
