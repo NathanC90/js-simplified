c03e0e9cd0c603a213c9c313c16e55edbd18ea57
NathanC90 <ymnchao@gmail.com>
2024-05-15 15:10:29 +0800

    Refactor code

--- 00-beginner/expand-collapse/script.js
+++ 00-beginner/expand-collapse/script.js
@@ -1,15 +1,30 @@
-const expandButtons = document.querySelectorAll(".expand-button")
+// const expandButtons = document.querySelectorAll(".expand-button")
 
-expandButtons.forEach(button => {
-    button.addEventListener("click", (e) => {
-        let currentButton = e.target
-        if(currentButton.innerHTML === "Expand") {
-            currentButton.innerHTML = "Collapse"
-        } else {
-            currentButton.innerHTML = "Expand"
-        }
-        const currentHeader = e.target.closest(".card-header")
-        const currentBody = currentHeader.nextElementSibling
-        currentBody.classList.toggle("show")
-    })
+// expandButtons.forEach(button => {
+//     button.addEventListener("click", (e) => {
+//         let currentButton = e.target
+//         if(currentButton.innerHTML === "Expand") {
+//             currentButton.innerHTML = "Collapse"
+//         } else {
+//             currentButton.innerHTML = "Expand"
+//         }
+//         const currentHeader = e.target.closest(".card-header")
+//         const currentBody = currentHeader.nextElementSibling
+//         currentBody.classList.toggle("show")
+//     })
+// })
+
+document.addEventListener("click", e => {
+    if(!e.target.matches(".expand-button")) return
+
+    const card = e.target.closest(".card")
+    const cardBody = card.querySelector(".card-body")
+
+    cardBody.classList.toggle("show")
+
+    if(e.target.innerText === "Expand") {
+        e.target.innerText = "Collapse"
+    } else {
+        e.target.innerText = "Expand"
+    }
 })
