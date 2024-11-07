ffaeb79f68ca7797ea25abf9143aa73ee03eb0ec
NathanC90 <35530607+NathanC90@users.noreply.github.com>
2024-11-07 15:54:09 +0800

    Update script.js
    refactor code

--- 00-beginner/expand-collapse/script.js
+++ 00-beginner/expand-collapse/script.js
@@ -1,30 +1,13 @@
-// const expandButtons = document.querySelectorAll(".expand-button")
-
-// expandButtons.forEach(button => {
-//     button.addEventListener("click", (e) => {
-//         let currentButton = e.target
-//         if(currentButton.innerHTML === "Expand") {
-//             currentButton.innerHTML = "Collapse"
-//         } else {
-//             currentButton.innerHTML = "Expand"
-//         }
-//         const currentHeader = e.target.closest(".card-header")
-//         const currentBody = currentHeader.nextElementSibling
-//         currentBody.classList.toggle("show")
-//     })
-// })
-
-document.addEventListener("click", e => {
-    if(!e.target.matches(".expand-button")) return
-
-    const card = e.target.closest(".card")
-    const cardBody = card.querySelector(".card-body")
-
-    cardBody.classList.toggle("show")
-
-    if(e.target.innerText === "Expand") {
-        e.target.innerText = "Collapse"
+const init = document.addEventListener("DOMContentLoaded", () => {
+  document.addEventListener("click", (e) => {
+    if (!e.target.matches(".expand-button")) return;
+    const card = e.target.closest(".card");
+    const cardBody = card.querySelector(".card-body");
+    cardBody.classList.toggle("show");
+    if (e.target.innerHTML === "Expand") {
+      e.target.innerHTML = "Collapse";
     } else {
-        e.target.innerText = "Expand"
+      e.target.innerHTML = "Expand";
     }
-})
+  });
+});
